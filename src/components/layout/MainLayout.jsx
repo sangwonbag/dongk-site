b0e3a2faf2c50b408aa395961977a1cc9a74b10c
sangwonbag <sangwonbag51@gmail.com>
2026-05-08 15:50:00 +0900

    chore: remove legacy AICounselor widget

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import FloatingPhoneButton from "../ui/FloatingPhoneButton";
 import RightFloatingBox from "../ui/RightFloatingBox";
-import AICounselor from "../ui/AICounselor";
 import "./MainLayout.css";
 
 export default function MainLayout({ children }) {
@@ -16,7 +15,6 @@ export default function MainLayout({ children }) {
             <Footer />
             <FloatingPhoneButton />
             <RightFloatingBox />
-            <AICounselor />
         </div>
     );
 }
